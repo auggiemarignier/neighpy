51b8253cc73cd69980323df0b60e1e7fefd6fd64
auggiemarignier <augustin.marignier@anu.edu.au>
2024-01-24 15:17:04 +1100

    test in 2D so can plot things

--- tests/test_napy.py
+++ tests/test_napy.py
@@ -15,28 +15,24 @@ def NAS():
     nr = 5
     ni = 5
     n = 20
-    bounds = ((0.0, 1.0), (0.0, 1.0), (0.0, 1.0))
+    bounds = ((0.0, 1.0), (0.0, 1.0))
     return NASearcher(objective, ns, nr, ni, n, bounds)
 
 
 def test__initial_random_search(NAS):
     new_samples = NAS._initial_random_search()
-    assert new_samples.shape == (NAS.ni, 3)
+    assert new_samples.shape == (NAS.ni, len(NAS.bounds))
 
     NAS._update_ensemble(new_samples)
     assert not np.array_equal(
-        NAS.samples[: NAS.ni], np.zeros((NAS.ni, 3))
+        NAS.samples[: NAS.ni], np.zeros((NAS.ni, len(NAS.bounds)))
     )
-    assert not np.array_equal(
-        NAS.objectives[: NAS.ni], np.zeros(NAS.ni)
-    )
+    assert not np.array_equal(NAS.objectives[: NAS.ni], np.zeros(NAS.ni))
 
     assert np.array_equal(
-        NAS.samples[NAS.ni :], np.zeros((NAS.nt - NAS.ni, 3))
+        NAS.samples[NAS.ni :], np.zeros((NAS.nt - NAS.ni, len(NAS.bounds)))
     )
-    assert np.array_equal(
-        NAS.objectives[NAS.ni :], np.full(NAS.nt - NAS.ni, np.inf)
-    )
+    assert np.array_equal(NAS.objectives[NAS.ni :], np.full(NAS.nt - NAS.ni, np.inf))
 
 
 def test__get_best_indices(NAS):
@@ -48,7 +44,7 @@ def test__get_best_indices(NAS):
     assert len(inds) == NAS.nr
     assert np.all(inds < NAS.ni)  # all indices should be from initial random search
     assert not np.array_equal(
-        NAS.samples[inds], np.zeros((NAS.nr, 3))
+        NAS.samples[inds], np.zeros((NAS.nr, len(NAS.bounds)))
     )  # all samples should be non-zero
     assert not np.array_equal(
         NAS.objectives[inds], np.zeros(NAS.nr)
