b1d6d112e7965cb8e82a5983cf300c15f7b286b1
auggiemarignier <augustin.marignier@anu.edu.au>
2024-01-24 15:28:27 +1100

    regular voronoi cells

--- tests/test_napy.py
+++ tests/test_napy.py
@@ -1,6 +1,10 @@
 import numpy as np
 from numpy.typing import ArrayLike
 import pytest
+from scipy.spatial import Voronoi
+from shapely.geometry import LineString
+from shapely.ops import polygonize
+import matplotlib.pyplot as plt
 
 from napy import NASearcher
 
@@ -49,3 +53,23 @@ def test__get_best_indices(NAS):
     assert not np.array_equal(
         NAS.objectives[inds], np.zeros(NAS.nr)
     )  # all objectives should be non-zero
+
+
+def test_random_walk_in_voronoi(NAS):
+    new_samples = np.meshgrid(
+        np.linspace(NAS.lower[0], NAS.upper[0], 5),
+        np.linspace(NAS.lower[1], NAS.upper[1], 5),
+    )
+    points = [
+        [x, y] for x, y in zip(new_samples[0].flatten(), new_samples[1].flatten())
+    ]
+    vor = Voronoi(points)
+    lines = [
+        LineString(vor.vertices[line]) for line in vor.ridge_vertices if -1 not in line
+    ]
+
+    polys = list(polygonize(lines))
+    for poly in polys:
+        plt.plot(*poly.exterior.xy, color="black")
+    plt.scatter(*np.array(points).T, color="red")
+    plt.show()
