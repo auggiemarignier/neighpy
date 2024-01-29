c89d52b528dccf0e95e122c56f65c193c396ce7d
auggiemarignier <augustin.marignier@anu.edu.au>
2024-01-29 18:20:21 +1100

    generator to yield intesection points

--- napy/_voronoi.py
+++ napy/_voronoi.py
@@ -11,12 +11,54 @@ class _VoronoiDiagram:
     between points and cell intersections with axes.
     """
 
-    def __init__(self, samples: ArrayLike, bounds: ArrayLike):
+    def __init__(
+        self, samples: ArrayLike, bounds: ArrayLike, weights: ArrayLike = None
+    ):
         self.cell_centres = samples
         self.bounds = bounds
         self.nd = len(bounds)
         self.lower = np.array([b[0] for b in bounds])
         self.upper = np.array([b[1] for b in bounds])
+        self.weights = weights if weights is not None else np.ones(self.nd)
+
+    def _cell_axis_intersections(self, vk: ArrayLike, k: int, i: int):
+        """
+        Calculate the intersection of the voronoi cell vk with the axis i.
+
+        Yields the intersection points vk.
+        """
+
+        # THIS IS WRONG - I DON'T THINK WE NEED THE i ARGUMENT SINCE WE YIELD
+        d2 = _VoronoiDiagram._distance(vk[i], self.cell_centres[:, i], self.weights[i])
+        d2 = np.sum(d2, axis=1)
+        d2_previous_axis = 0  # distance to previous axis
+
+        for j in range(self.nd):
+            d2_current_axis = _VoronoiDiagram._distance(
+                vk[j], self.cell_centres[:, j], self.weights[j]
+            )
+            d2 += d2_previous_axis - d2_current_axis
+            d2_previous_axis = d2_current_axis
+            dk2 = d2[k]  # disctance of cell centre to axis
+
+            # eqn (19) Sambridge 1999
+            vji = self.cell_centres[:, j]
+            vki = vk[j]
+            a = dk2 - d2
+            b = vki - vji
+            xji = 0.5 * (
+                vki + vji + np.divide(a, b, out=np.zeros_like(a), where=b != 0)
+            )
+            d2_previous_axis = d2_current_axis
+
+            yield xji
+
+    def _update_diagram(self, samples):
+        self.cell_centres = samples
+
+    @staticmethod
+    def _distance(x1, x2, w):
+        return w * (x1 - x2) ** 2
 
 
 def _random_walk_in_voronoi(self, vk: ArrayLike, k: int) -> ArrayLike:
