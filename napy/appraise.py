65f93b0d82db1ec6170019c86075b7e55e3aabe0
auggiemarignier <augustin.marignier@anu.edu.au>
2024-01-31 10:35:26 +1100

    get axis intersections

--- napy/appraise.py
+++ napy/appraise.py
@@ -17,6 +17,7 @@ class NAAppariser:
         self.nd = len(bounds)
         self.lower = np.array([b[0] for b in bounds])
         self.upper = np.array([b[1] for b in bounds])
+        self.Cm = 1 / (self.upper - self.lower) ** 2
 
         self.Ne = len(initial_ensemble)
         self.Nr = n_resample
@@ -44,18 +45,68 @@ class NAAppariser:
                 accepted = True
                 return xpi
 
-    def axis_intersections(self, axis: int, vk: ArrayLike, k: int):
+    def axis_intersections(self, axis: int, k: int):
         """
         Calculate the intersections of an axis passing through point vk in the kth cell
         with the boundaries of all cells
 
-        Yields the intersection points.
+        Returns the intersection points.
         """
 
+        down = self._travel_along_axis(axis, k, down=True)
+        up = self._travel_along_axis(axis, k, up=True)
+
+        return np.array(sorted(down + up))
+
+    def _travel_along_axis(
+        self, axis: int, k: int, down: bool = False, up: bool = False
+    ) -> list:
+        intersections = []
+        next_cell = k
+        bound_reached = False
+        while not bound_reached:
+            _next_cell, intersection, bound_reached = self._get_axis_intersection(
+                axis, next_cell, down=down, up=up
+            )
+            if intersection is not None and _next_cell is not None:
+                intersections.append(intersection)
+                next_cell = _next_cell
+            else:
+                break
+        return intersections
+
+    def _get_axis_intersection(
+        self, axis: int, k: int, down: bool = False, up: bool = False
+    ):
+        """
+        Returns the index of the next cell along the axis that shares a boundary with the kth cell,
+        the intersection point, and whether the bound is reached.
+        """
         # eqn (19) Sambridge 1999
-        d2 = np.sum(self.weights * (vk - self.initial_ensemble) ** 2, axis=1)
-        vji = self.initial_ensemble[:, axis]
+        vk = self.initial_ensemble[k]
         vki = vk[axis]
-        a = d2[k] - d2
-        b = vki - vji
+        vji = self.initial_ensemble[:, axis]
+        d2i = (
+            np.sum(self.Cm * (vk - self.initial_ensemble) ** 2, axis=1)
+            - self.Cm[axis] * (vki - vji) ** 2
+        )  # perpendicular distance to current axis
+        a = d2i[k] - d2i
+        b = self.Cm[axis] * (vki - vji)
         xji = 0.5 * (vki + vji + np.divide(a, b, out=np.zeros_like(a), where=b != 0))
+
+        if down:
+            xji = np.ma.array(xji, mask=vki <= vji)
+            if xji.count() > 0:  # valid intersections found
+                k_new = np.argmax(xji)  # closest to vk
+                return k_new, xji[k_new], False
+            else:
+                return None, None, True
+        elif up:
+            xji = np.ma.array(xji, mask=vki >= vji)
+            if xji.count() > 1:
+                k_new = np.argmin(xji)
+                return k_new, xji[k_new], False
+            else:
+                return None, None, True
+        else:
+            raise ValueError("Must specify up or down")
