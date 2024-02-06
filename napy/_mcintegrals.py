85198d8aaeb96238cbe161fd95b6e37c82975169
auggiemarignier <augustin.marignier@anu.edu.au>
2024-02-07 10:43:13 +1100

    allow accumulator to accumulate other accumulators

--- napy/_mcintegrals.py
+++ napy/_mcintegrals.py
@@ -1,5 +1,7 @@
+from __future__ import annotations  # needed for type hint of self in accumulate
 import numpy as np
 from numpy.typing import ArrayLike
+from typing import Union
 
 
 class MCIntegrals:
@@ -17,7 +19,15 @@ class MCIntegrals:
         self.N = 0
         self.samples = [] if save_sammples else None
 
-    def accumulate(self, x: ArrayLike):
+    def accumulate(self, x: Union[ArrayLike, MCIntegrals]):
+        if isinstance(x, np.ndarray):
+            # usual case
+            self._accumulate_arraylike(x)
+        else:
+            # e.g. accumulating multiple parallel walkers each with their own accumulator
+            self._accumulate_mcintegrals(x)
+
+    def _accumulate_arraylike(self, x: ArrayLike):
         self.mi += x
         self.mi2 += x**2
         self.mimj += np.outer(x, x)
@@ -27,6 +37,17 @@ class MCIntegrals:
         self.N += 1
         if self.samples is not None:
             self.samples.append(x)
+
+    def _accumulate_mcintegrals(self, x: MCIntegrals):
+        self.mi += x.mi
+        self.mi2 += x.mi2
+        self.mimj += x.mimj
+        self.mi2mj += x.mi2mj
+        self.mimj2 += x.mimj2
+        self.mi2mj2 += x.mi2mj2
+        self.N += x.N
+        if self.samples is not None and x.samples is not None:
+            self.samples.extend(x.samples)
 
     def mean(self):
         return self.mi / self.N
--- tests/test__mcintegrals.py
+++ tests/test__mcintegrals.py
@@ -32,6 +32,22 @@ def test_accumulate(MCIntegrals_obj):
     assert np.allclose(MCIntegrals_obj.mi2mj2, [[82.0, 148.0], [148.0, 272.0]])
 
 
+def test_accumulate_objects(MCIntegrals_obj):
+    x = np.array([1.0, 2.0])
+    MCIntegrals_obj.accumulate(x)
+    x = np.array([3.0, 4.0])
+    MCIntegrals_obj2 = MCIntegrals(2)
+    MCIntegrals_obj2.accumulate(x)
+    MCIntegrals_obj.accumulate(MCIntegrals_obj2)
+    assert MCIntegrals_obj.N == 2
+    assert np.allclose(MCIntegrals_obj.mi, [4.0, 6.0])
+    assert np.allclose(MCIntegrals_obj.mi2, [10.0, 20.0])
+    assert np.allclose(MCIntegrals_obj.mimj, [[10.0, 14.0], [14.0, 20.0]])
+    assert np.allclose(MCIntegrals_obj.mi2mj, [[28.0, 38.0], [52.0, 72.0]])
+    assert np.allclose(MCIntegrals_obj.mimj2, [[28.0, 52.0], [38.0, 72.0]])
+    assert np.allclose(MCIntegrals_obj.mi2mj2, [[82.0, 148.0], [148.0, 272.0]])
+
+
 def test_mean(MCIntegrals_obj):
     samples = np.random.rand(10, 2)
     for x in samples:
