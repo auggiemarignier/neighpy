c386fb9226dc902cdb33b6be4e527df0dd4cf7cf
auggiemarignier <augustin.marignier@anu.edu.au>
2024-02-07 10:20:15 +1100

    fix copilot tests

--- tests/test__mcintegrals.py
+++ tests/test__mcintegrals.py
@@ -14,38 +14,37 @@ def test_accumulate(MCIntegrals_obj):
     x = np.array([1.0, 2.0])
     MCIntegrals_obj.accumulate(x)
     assert MCIntegrals_obj.N == 1
-    assert MCIntegrals_obj.mi == pytest.approx(x)
-    assert MCIntegrals_obj.mi2 == pytest.approx(x**2)
-    assert MCIntegrals_obj.mimj == pytest.approx(np.outer(x, x))
-    assert MCIntegrals_obj.mi2mj == pytest.approx(np.outer(x**2, x))
-    assert MCIntegrals_obj.mimj2 == pytest.approx(np.outer(x, x**2))
-    assert MCIntegrals_obj.mi2mj2 == pytest.approx(np.outer(x**2, x**2))
+    assert np.allclose(MCIntegrals_obj.mi, x)
+    assert np.allclose(MCIntegrals_obj.mi2, x**2)
+    assert np.allclose(MCIntegrals_obj.mimj, np.outer(x, x))
+    assert np.allclose(MCIntegrals_obj.mi2mj, np.outer(x**2, x))
+    assert np.allclose(MCIntegrals_obj.mimj2, np.outer(x, x**2))
+    assert np.allclose(MCIntegrals_obj.mi2mj2, np.outer(x**2, x**2))
 
     x = np.array([3.0, 4.0])
     MCIntegrals_obj.accumulate(x)
     assert MCIntegrals_obj.N == 2
-    assert MCIntegrals_obj.mi == pytest.approx([4.0, 6.0])
-    assert MCIntegrals_obj.mi2 == pytest.approx([10.0, 20.0])
-    assert MCIntegrals_obj.mimj == pytest.approx([[3.0, 4.0], [6.0, 8.0]])
-    assert MCIntegrals_obj.mi2mj == pytest.approx([[9.0, 12.0], [12.0, 16.0]])
-    assert MCIntegrals_obj.mimj2 == pytest.approx([[3.0, 8.0], [12.0, 32.0]])
-    assert MCIntegrals_obj.mi2mj2 == pytest.approx([[9.0, 24.0], [24.0, 64.0]])
+    assert np.allclose(MCIntegrals_obj.mi, [4.0, 6.0])
+    assert np.allclose(MCIntegrals_obj.mi2, [10.0, 20.0])
+    assert np.allclose(MCIntegrals_obj.mimj, [[10.0, 14.0], [14.0, 20.0]])
+    assert np.allclose(MCIntegrals_obj.mi2mj, [[28.0, 38.0], [52.0, 72.0]])
+    assert np.allclose(MCIntegrals_obj.mimj2, [[28.0, 52.0], [38.0, 72.0]])
+    assert np.allclose(MCIntegrals_obj.mi2mj2, [[82.0, 148.0], [148.0, 272.0]])
 
 
 def test_mean(MCIntegrals_obj):
-    x = np.array([1.0, 2.0])
-    MCIntegrals_obj.accumulate(x)
-    x = np.array([3.0, 4.0])
-    MCIntegrals_obj.accumulate(x)
-    assert MCIntegrals_obj.mean() == pytest.approx([2.0, 3.0])
+    samples = np.random.rand(10, 2)
+    for x in samples:
+        MCIntegrals_obj.accumulate(x)
+
+    assert np.allclose(MCIntegrals_obj.mean(), samples.mean(axis=0))
 
 
 def test_covariance(MCIntegrals_obj):
-    x = np.array([1.0, 2.0])
-    MCIntegrals_obj.accumulate(x)
-    x = np.array([3.0, 4.0])
-    MCIntegrals_obj.accumulate(x)
-    mean = MCIntegrals_obj.mean()
-    assert MCIntegrals_obj.covariance() == pytest.approx(
-        [[1.0, 1.0], [1.0, 1.0]]
-    ) - np.outer(mean, mean)
+    samples = np.random.rand(10, 2)
+    for x in samples:
+        MCIntegrals_obj.accumulate(x)
+
+    assert np.allclose(
+        MCIntegrals_obj.covariance(), np.cov(samples, rowvar=False, bias=True)
+    )
